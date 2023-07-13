74da96504cad2a59b857bf30e7ebd6dd049a6be3
dedenurr <dedenurrahman.git@gmail.com>
2023-07-13 15:39:19 +0700

    update user types

--- pkg/api/rest/handler_user.go
+++ pkg/api/rest/handler_user.go
@@ -12,7 +12,6 @@ import (
 	"github.com/abialemuel/ymirblog/pkg/persist/ymirblog"
 	"github.com/kubuskotak/asgard/rest"
 	pkgRest "github.com/kubuskotak/asgard/rest"
-	pkgTracer "github.com/kubuskotak/asgard/tracer"
 )
 
 // UserOption is a struct holding the handler options.
@@ -56,8 +55,7 @@ func WithUserUsecase(u user.T) UserOption {
 func (u *User) Register(router chi.Router) {
 	// PLEASE EDIT THIS EXAMPLE, how to register handler to router
 	router.Post("/users", pkgRest.HandlerAdapter[CreateUserRequest](u.CreateUser).JSON)
-	router.Get("/hello", pkgRest.HandlerAdapter[GetUserRequest](u.GetUser).JSON)
-	router.Get("/hello-csv", pkgRest.HandlerAdapter[GetUserCSVRequest](u.GetUserCSV).CSV)
+
 }
 
 // Create User handler
@@ -89,32 +87,3 @@ func (u *User) CreateUser(w http.ResponseWriter, r *http.Request) (GetUserRespon
 	}, nil
 }
 
-// GetUser endpoint func. /** PLEASE EDIT THIS EXAMPLE, return handler response */.
-func (u *User) GetUser(w http.ResponseWriter, r *http.Request) (GetUserResponse, error) {
-	_, span, l := pkgTracer.StartSpanLogTrace(r.Context(), "User")
-	defer span.End()
-
-	l.Info().Str("Hello", "World").Msg("this")
-
-	return GetUserResponse{
-		Message: "Hello everybody",
-	}, nil
-}
-
-// GetUserCSV endpoint func. /** PLEASE EDIT THIS EXAMPLE, return handler response */.
-func (u *User) GetUserCSV(w http.ResponseWriter, r *http.Request) (pkgRest.ResponseCSV, error) {
-	_, span, l := pkgTracer.StartSpanLogTrace(r.Context(), "UserCSV")
-	defer span.End()
-
-	l.Info().Str("Hello", "World").Msg("this")
-
-	rows := make([][]string, 0)
-	rows = append(rows, []string{"SO Number", "Nama Warung", "Area", "Fleet Number", "Jarak Warehouse", "Urutan"})
-	rows = append(rows, []string{"SO45678", "WPD00011", "Jakarta Selatan", "1", "45.00", "1"})
-	rows = append(rows, []string{"SO45645", "WPD001123", "Jakarta Selatan", "1", "43.00", "2"})
-	rows = append(rows, []string{"SO45645", "WPD003343", "Jakarta Selatan", "1", "43.00", "3"})
-	return pkgRest.ResponseCSV{
-		Filename: "warehouse",
-		Rows:     rows,
-	}, nil
-}
--- pkg/api/rest/handler_user_types.go
+++ pkg/api/rest/handler_user_types.go
@@ -5,9 +5,7 @@ import "github.com/abialemuel/ymirblog/pkg/entity"
 
 // GetUserRequest Get a User request.  /** PLEASE EDIT THIS EXAMPLE, request handler */.
 type GetUserRequest struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name" validate:"required"`
-	Email string `json:"email" validate:"email,required"`
+
 }
 
 // GetUserResponse Get a User response.  /** PLEASE EDIT THIS EXAMPLE, return handler response */.
